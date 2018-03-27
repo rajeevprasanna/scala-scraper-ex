45508cf875976e9da9ef804fdafd43be22fe0d3f
rajeevprasanna <rajeevprasanna@gmail.com>
2018-03-27 18:20:27 +0530

    code refactoring

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,14 @@
+import java.util.concurrent.TimeoutException
+
 import AppContext._
 import Models.FileMetaData
 import com.typesafe.scalalogging.LazyLogging
+import org.openqa.selenium.net.UrlChecker.TimeoutException
 
-import scala.concurrent.Future
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success, Try}
 
 
 object Main extends App with LazyLogging {
@@ -14,35 +19,34 @@ object Main extends App with LazyLogging {
   val maxDownloadFiles = config.getInt("crawl.max_download_files")
 
   def startWebCrawler():Unit = {
-    BFRedisClient.fetchCrawlUrl().flatMap(urlOp => urlOp match {
-      case Some(urlPayload) =>
-        Crawler.extractFiles(urlPayload.url, maxDepth, maxDownloadFiles, urlPayload.is_ajax.getOrElse(false))
-        startWebCrawler()
-        Future{}
+    Try(Await.result(BFRedisClient.fetchCrawlUrl(), 10 seconds)) match {
+      case Success(Some(urlPayload)) =>
+            logger.info(s"Going to start crawling for url => ${urlPayload.url} with ajax status => ${urlPayload.is_ajax}")
+            Crawler.extractFiles(urlPayload.url, maxDepth, maxDownloadFiles, urlPayload.is_ajax.getOrElse(false))
+            startWebCrawler()
 
-      case None =>
-        //TODO : Add delay function
-        Thread.sleep(5000)
+      case Failure(_) =>
+        logger.info(s"No crawl url in redis for crawl ")
         startWebCrawler()
-        Future{}
-    })
+    }
   }
 
 
   def startFileDownloader():Unit = {
-      BFRedisClient.fetchResourceUrlsPayload().map(crawlPayloadOp => crawlPayloadOp match {
-        case Some(payload) =>
-            val filteredUrls:Future[List[String]] = BFService.filterProcessedUrls(payload.resourceUrl, payload.urls)
-            filteredUrls.map(validUrls => {
-              val filesMatadata:List[FileMetaData] = validUrls.flatMap(fileUrl => FileUtils.uploadResource(fileUrl, payload.pageUrl.getOrElse("")))
-              if(!filesMatadata.isEmpty) BFService.uploadFilesMetadata(payload.resourceUrl, filesMatadata)
-              if(payload.completed) BFService.markProcessComplete(payload.resourceUrl)
-              startFileDownloader()
-            })
-
-        case None =>
+    Try(Await.result(BFRedisClient.fetchResourceUrlsPayload(), 10 seconds)) match {
+      case Success(Some(payload)) =>
+        val filteredUrls:Future[List[String]] = BFService.filterProcessedUrls(payload.resourceUrl, payload.urls)
+        filteredUrls.map(validUrls => {
+          val filesMatadata:List[FileMetaData] = validUrls.flatMap(fileUrl => FileUtils.uploadResource(fileUrl, payload.pageUrl.getOrElse("")))
+          if(!filesMatadata.isEmpty) BFService.uploadFilesMetadata(payload.resourceUrl, filesMatadata)
+          if(payload.completed) BFService.markProcessComplete(payload.resourceUrl)
           startFileDownloader()
-      })
+        })
+
+      case Failure(_) =>
+        logger.info(s"No resource  url in redis for downloading")
+        startFileDownloader()
+    }
   }
 
 
