3e0c9e03f8ed2243385a6d552c0886081a428197
rajeevprasanna <rajeevprasanna@gmail.com>
2018-03-29 18:47:32 +0530

    adding blacklisted country extensions

--- src/main/scala/DomUtils.scala
+++ src/main/scala/DomUtils.scala
@@ -154,7 +154,7 @@ object DomUtils {
 
     val invalidExtensions = Set("cz","dk","fi","fr","de","gr","hu","it","nl","no","pl","ru","es","se","tr","uk","au","cn","hk","jp","kr","my","ph","sg","th", "ja_jp", "de_de", "br", "lat", "tw")
     val blackListedURLPatterns = Set("community.", "/community/", "communities", "CommunityBlog", "discussions.", "forums/", "javascript:", "mailto:", "tel:", "careers.", "google.","instagram.","linkedin.","tumblr.","twitter.","fb.","facebook.","youtube.", "/careers","/facilities/", "pinterest.","yahoo.", "JPTraps")
-    val blackListedcountryExtensions = invalidExtensions.map("/"+_+"/") ++ blackListedURLPatterns
+    val blackListedcountryExtensions = invalidExtensions.map("/"+_+"/") ++ blackListedURLPatterns ++ invalidExtensions.map(_+".") //checking for patterns like jp.x.com etc
     def containsBlackListedUrl = (url:String) => blackListedcountryExtensions.map(url.contains(_)).collectFirst({case x if x == true => x}).getOrElse(false)
 
     urls.filter(url => !invalidExtensions.contains(getDomainCountryExtension(url)) && !invalidExtensions.contains(getCountryRoute(url)) && !containsBlackListedUrl(url))
