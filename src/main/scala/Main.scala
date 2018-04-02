c07f22ec0624d45123a3a7b4d7c659d6cadc8d54
rajeevprasanna <rajeevprasanna@gmail.com>
2018-04-02 16:38:41 +0530

    adding temporary hack

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -15,22 +15,28 @@ object Main extends App with LazyLogging with AppContext {
   val maxDepth = config.getInt("crawl.depth")
   val maxDownloadFiles = config.getInt("crawl.max_download_files")
 
+  var crawlInProgress = false
+
   def startWebCrawler():Unit = {
-    Try(Await.result(BFRedisClient.fetchCrawlUrl(), 10 seconds)) match {
-      case Success(Some(urlPayload)) =>
-            logger.info(s"Going to start crawling for url => ${urlPayload.url} with ajax status => ${urlPayload.is_ajax}")
-            Crawler.extractFiles(urlPayload.url, maxDepth, maxDownloadFiles, urlPayload.is_ajax.getOrElse(false)).map(_ => startWebCrawler())
+    if(!crawlInProgress){
+      Try(Await.result(BFRedisClient.fetchCrawlUrl(), 10 seconds)) match {
+        case Success(Some(urlPayload)) =>
+          crawlInProgress = true
+          logger.info(s"Going to start crawling for url => ${urlPayload.url} with ajax status => ${urlPayload.is_ajax}")
+          Try(Crawler.extractFiles(urlPayload.url, maxDepth, maxDownloadFiles, urlPayload.is_ajax.getOrElse(false))).map(_ => {
+            crawlInProgress = false
+          })
 
-      case Failure(_) =>
-        logger.info(s"No crawl url in redis for crawl ")
-        startWebCrawler()
+        case Failure(_) =>
+          logger.info(s"No crawl url in redis for crawl ")
 
-      case x =>
-        logger.error(s"seems reached invalid state => $x")
-        startWebCrawler()
+        case x =>
+          logger.error(s"seems reached invalid state => $x")
+      }
     }
+    Thread.sleep(2000)
+    startWebCrawler()
   }
-
 
   def startFileDownloader():Unit = {
     def reduceMetadata = (list:List[Option[FileMetaData]], a2:Option[FileMetaData]) => list :+ a2
