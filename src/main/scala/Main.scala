6b6916cde3714d9af60ca3356fc9eda60816961c
rajeevprasanna <rajeevprasanna@gmail.com>
2018-04-15 23:39:08 +0530

    adding comment

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,4 +6,5 @@ import scala.concurrent.Future
 object Main extends App with LazyLogging with AppContext {
   Future{CrawlTrigger.startCrawling()}
   Future{FileDownloadTrigger.startFileDownloader()}
+//  Crawler.extractFiles("https://www.box.com", ConfReader.maxCrawlDepth, ConfReader.maxNeededFiles, false)
 }
